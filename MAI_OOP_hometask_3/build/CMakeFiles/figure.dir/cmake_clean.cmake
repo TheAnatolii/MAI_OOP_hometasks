a7d0809204a13cc5173eafcae01366d345e17789
AnatoliiG <avgolovenko@mai.education>
2023-10-22 16:45:35 +0300

    update

---
+++ MAI_OOP_hometask_3/build/CMakeFiles/figure.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/figure.dir/eight_angles.cpp.o"
+  "CMakeFiles/figure.dir/eight_angles.cpp.o.d"
+  "CMakeFiles/figure.dir/five_angles.cpp.o"
+  "CMakeFiles/figure.dir/five_angles.cpp.o.d"
+  "CMakeFiles/figure.dir/main.cpp.o"
+  "CMakeFiles/figure.dir/main.cpp.o.d"
+  "CMakeFiles/figure.dir/mass.cpp.o"
+  "CMakeFiles/figure.dir/mass.cpp.o.d"
+  "CMakeFiles/figure.dir/six_angles.cpp.o"
+  "CMakeFiles/figure.dir/six_angles.cpp.o.d"
+  "figure"
+  "figure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/figure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

b1f4cec234af74d46aa6b91d072878f56727ed74
AnatoliiG <avgolovenko@mai.education>
2023-11-12 16:35:40 +0300

    remove build

--- MAI_OOP_hometask_4/build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anatolii/Desktop/OOP_MAI/OOP_hometasks/MAI_OOP_hometask_4/googletest/googletest
-# Build directory: /Users/anatolii/Desktop/OOP_MAI/OOP_hometasks/MAI_OOP_hometask_4/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

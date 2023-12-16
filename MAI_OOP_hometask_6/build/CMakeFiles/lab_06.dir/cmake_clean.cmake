939f5c0d8e0ddf448aa2146d0154dc6f323b687e
AnatoliiG <avgolovenko@mai.education>
2023-12-17 00:21:15 +0300

    add files lab 6

---
+++ MAI_OOP_hometask_6/build/CMakeFiles/lab_06.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_06.dir/NPC.cpp.o"
+  "CMakeFiles/lab_06.dir/NPC.cpp.o.d"
+  "CMakeFiles/lab_06.dir/bear.cpp.o"
+  "CMakeFiles/lab_06.dir/bear.cpp.o.d"
+  "CMakeFiles/lab_06.dir/elf.cpp.o"
+  "CMakeFiles/lab_06.dir/elf.cpp.o.d"
+  "CMakeFiles/lab_06.dir/factory.cpp.o"
+  "CMakeFiles/lab_06.dir/factory.cpp.o.d"
+  "CMakeFiles/lab_06.dir/main.cpp.o"
+  "CMakeFiles/lab_06.dir/main.cpp.o.d"
+  "CMakeFiles/lab_06.dir/observer.cpp.o"
+  "CMakeFiles/lab_06.dir/observer.cpp.o.d"
+  "CMakeFiles/lab_06.dir/thug.cpp.o"
+  "CMakeFiles/lab_06.dir/thug.cpp.o.d"
+  "lab_06"
+  "lab_06.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
